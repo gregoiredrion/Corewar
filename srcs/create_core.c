d7020d58298f944c46fb64648dd1e1e73d922dbe
Bill <williamdeltenre@gmail.com>
2019-12-21 15:30:08 +0100

    fix merge

--- srcs/create_core.c
+++ srcs/create_core.c
@@ -6,11 +6,7 @@
 /*   By: wdeltenr <wdeltenr@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/11 14:00:41 by wdeltenr          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2019/12/21 10:41:55 by gregoiredrion    ###   ########.fr       */
-=======
 /*   Updated: 2019/12/17 19:50:50 by wdeltenr         ###   ########.fr       */
->>>>>>> 649a97267cfc54757606e15dbaaa3e78d0ec773c
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +21,7 @@ int			create_cor(t_cor *cor)
 	cor->comment = NULL;
 	cor->carry = 0;
 	cor->size = 0;
-<<<<<<< HEAD
 	cor->line = 1;
-	return (1);
-=======
 	cor->line = 0;
 	return (OK);
->>>>>>> 649a97267cfc54757606e15dbaaa3e78d0ec773c
 }
