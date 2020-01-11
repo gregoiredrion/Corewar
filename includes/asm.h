03da46eb7b2625e1174f952161f7289cd3fa012a
William Deltenre <williamdeltenre@gmail.com>
2020-01-11 01:41:29 +0100

    asm.h structure

--- includes/asm.h
+++ includes/asm.h
@@ -6,7 +6,7 @@
 /*   By: wdeltenr <wdeltenr@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/10 15:27:46 by wdeltenr          #+#    #+#             */
-/*   Updated: 2020/01/10 23:55:30 by wdeltenr         ###   ########.fr       */
+/*   Updated: 2020/01/11 01:41:11 by wdeltenr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,7 +66,7 @@ typedef struct	s_cor
 	int				fd;
 	size_t			flag;
 	char			*file;
-	t_arg_type	tab[3];
+	t_arg_type		tab[3];
 	size_t			size;
 	size_t			line;
 	size_t			pos;
@@ -79,55 +79,59 @@ typedef struct	s_cor
 	header_t		header;
 }				t_cor;
 
-int		usage(void);
-int		error_msg(int ret);
-int		create_cor(t_cor *cor);
-int		check_name_comment(t_cor *cor, int fd);// unused ftm
-int		init_cor(t_cor *cor, char **argv);
-int		empty(char *line);
-int		upper(char *str);
-int		blank(char *str);
+/*
+** Parsing
+*/
+int			asm_parser(t_cor *cor, char *file);
+t_token		*create_token(char *input, int type, size_t col, size_t line);
+int			create_cor(t_cor *cor);
+int			skip_nl(t_cor *cor, char *input, size_t *line, size_t *col);
+int			tokenisation(char *input);
+int			check_ind(char *input, size_t i);
+int			check_label(char *input);
+int			check_cmd(char *input);
+int			check_dir(char *input);
+int			check_reg_instr(char *input);
+int			token_validity(t_cor *cor);
+int			get_instr(char *input, size_t *n);
+int			get_reg(char *input, size_t *n);
+int			get_indir(char *input, size_t *n);
+int			get_type(char *input, size_t *n);
+int			tokenization(t_cor *cor, char *input, size_t *col, size_t line);
+void		pushback_token(t_cor *cor, t_token *token);
+int			upper(char *str);
 
-int		check_format_arg(char *arg, int type);
+/*
+** Error
+*/
+int			usage(void);
+int			invalid_param(char *instr, int type, int param, size_t pos);
+int			lexical_error(size_t line, size_t col);
+t_token		*syntax_error(t_token *token);
+int			invalid_instr(t_token *token);
+int			error_msg(int ret);
+
+/*
+** Store and process tokens to prog
+*/
 t_token		*store_instruction(t_cor *cor, t_token *token);
-//int		store_params(t_cor *cor, char *params);
 t_token		*store_label(t_cor *cor, t_token *label);
 t_token		*store_cmd(t_cor *cor, t_token *token);
 t_token		*store_params(t_cor *cor, t_token *token);
-int		store_header(t_cor *cor, t_token *token);
+int			last_offsets(t_cor *cor);
+int			offsets(t_cor *cor, t_token *token, size_t nb_bytes);
+t_token		*store_comment(t_cor *cor, t_token *token);
+t_token		*store_name(t_cor *cor, t_token *token);
+void		write_prog(t_cor *cor, int add, size_t nb_bytes);
+int			process_tokens(t_cor *cor);
+int			instr_params(t_cor *cor, t_token *token, size_t nb_arg);
+
+
+/*
+** Output
+*/
 char	*create_cor_file(t_cor *cor);
-int 	asm_parser(t_cor *cor, char *file);
-int		trim_file(char **file);
-int		skip_nl(t_cor *cor, char *input, size_t *line, size_t *col);
-t_token	*create_token(char *input, int type, size_t col, size_t line);
-void	pushback_token(t_cor *cor, t_token *token);
 
-int		invalid_param(char *instr, int type, int param, size_t pos);
-int		lexical_error(size_t line, size_t col);
-t_token	*syntax_error(t_token *token);
-int		invalid_instr(t_token *token);
-
-int		last_offsets(t_cor *cor);
-int		tokenisation(char *input);
-int		check_ind(char *input, size_t i);
-int		check_label(char *input);
-int		check_cmd(char *input);
-int		check_dir(char *input);
-int		check_reg_instr(char *input);
-int		split_input(t_cor *cor, char *input, size_t i, size_t line);
-int		token_validity(t_cor *cor);
-t_token	*check_params(t_cor *cor, t_token *token);
-int		offsets(t_cor *cor, t_token *token, size_t nb_bytes);
-t_token	*store_comment(t_cor *cor, t_token *token);
-t_token	*store_name(t_cor *cor, t_token *token);
-void	write_prog(t_cor *cor, int add, size_t nb_bytes);
-int		get_instr(char *input, size_t *n);
-int		get_reg(char *input, size_t *n);
-int		get_indir(char *input, size_t *n);
-int		get_type(char *input, size_t *n);
-int		tokenization(t_cor *cor, char *input, size_t *col, size_t line);
-int		process_tokens(t_cor *cor);
-int		instr_params(t_cor *cor, t_token *token, size_t nb_arg);
 //display_tokens
 void			display_tokens(t_token *tokens);
 
